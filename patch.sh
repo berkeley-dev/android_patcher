ee3305890c954f2aa62681ce7e843498b93b03d2
luk1337 <priv.luk@gmail.com>
2018-12-06 04:07:12 +0100

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -8,7 +8,6 @@ repopick 227124 # BatteryService: Add support for oem fast charger detection
 
 # frameworks/opt/telephony
 repopick 227125 # RIL: Allow overriding RadioResponse and RadioIndication
-repopick 229605 # Telephony: Don't call onUssdRelease for Huawei RIL
 
 # packages/services/Telephony
-repopick 229610 # Telephony: Support muting by RIL command on Huawei RIL
+repopick 229610 # Telephony: Support muting by RIL command
