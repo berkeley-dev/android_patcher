7a6e8bc5965ba7032d49bee8a2893431d87a6dd2
LuK1337 <priv.luk@gmail.com>
2018-12-06 14:13:22 +0100

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -2,6 +2,9 @@
 
 . build/envsetup.sh
 
+# build
+repopick 235958 # build: Support systam-as-root A-only backuptool
+
 # frameworks/base
 repopick 229606 # ConsumerIR: Support Huawei's DSP chip implementation
 repopick 227124 # BatteryService: Add support for oem fast charger detection
@@ -11,3 +14,6 @@ repopick 227125 # RIL: Allow overriding RadioResponse and RadioIndication
 
 # packages/services/Telephony
 repopick 229610 # Telephony: Support muting by RIL command
+
+# vendor/lineage
+repopick 235959 # lineage: Support systam-as-root A-only backuptool
