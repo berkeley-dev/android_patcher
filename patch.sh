f9fa873e890ba14e8763013b4d7aeea923e2c566
luk1337 <priv.luk@gmail.com>
2018-12-18 11:23:46 +0100

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -5,9 +5,3 @@
 # frameworks/base
 repopick 229606 # ConsumerIR: Support Huawei's DSP chip implementation
 repopick 227124 # BatteryService: Add support for oem fast charger detection
-
-# frameworks/opt/telephony
-repopick 227125 # RIL: Allow overriding RadioResponse and RadioIndication
-
-# packages/services/Telephony
-repopick 229610 # Telephony: Support muting by RIL command
