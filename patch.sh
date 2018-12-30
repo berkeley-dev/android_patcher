f9671621c32f4d113850e53ac8dc0854e2ce2126
luk1337 <priv.luk@gmail.com>
2018-12-30 19:28:11 +0100

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
 . build/envsetup.sh
-
-# frameworks/base
-repopick 229606 # ConsumerIR: Support Huawei's DSP chip implementation
-repopick 227124 # BatteryService: Add support for oem fast charger detection
